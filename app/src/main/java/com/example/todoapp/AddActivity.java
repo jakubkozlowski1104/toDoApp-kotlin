011ead7e03fb18709493be78dad70836bb80ee13
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-24 16:56:42 +0200

    connect to db

--- app/src/main/java/com/example/todoapp/AddActivity.java
+++ app/src/main/java/com/example/todoapp/AddActivity.java
@@ -1,24 +1,47 @@
 package com.example.todoapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText titleInput, descriptionInput;
+    private Spinner spinner;
+    private Button addTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        titleInput = findViewById(R.id.title_input);
+        descriptionInput = findViewById(R.id.description_input);
+        spinner = findViewById(R.id.spinner);
+        addTaskButton = findViewById(R.id.addTask_button);
+
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pobranie wartości z pól wejściowych
+                String title = titleInput.getText().toString().trim();
+                String description = descriptionInput.getText().toString().trim();
+                String category = spinner.getSelectedItem().toString();
+
+                if (!title.isEmpty() && !description.isEmpty()) {
+                    MyDatabaseHelper dbHelper = new MyDatabaseHelper(AddActivity.this);
+                    dbHelper.addTask(title, description, Category.valueOf(category.toUpperCase())); // Zamiana na wielkie litery
+                    Toast.makeText(AddActivity.this, "Task added successfully", Toast.LENGTH_SHORT).show();
+                    finish(); // Zakończenie aktywności po dodaniu zadania
+                } else {
+                    Toast.makeText(AddActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/example/todoapp/MyDatabaseHelper.java
+++ app/src/main/java/com/example/todoapp/MyDatabaseHelper.java
@@ -6,19 +6,23 @@
 
 package com.example.todoapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
     // Database Name and Version
+
+    private Context context;
     private static final String DATABASE_NAME = "todoApp.db";
     private static final int DATABASE_VERSION = 1;
 
     // Table Name
-    private static final String TABLE_TASKS = "tasks";
+    private static final String TABLE_NAME = "tasks";
 
     // Tasks Table Columns
     private static final String COLUMN_ID = "id";
@@ -33,12 +37,13 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Create tasks table
-        String createTasksTable = "CREATE TABLE " + TABLE_TASKS + " ("
+        String createTasksTable = "CREATE TABLE " + TABLE_NAME + " ("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COLUMN_TITLE + " TEXT, "
                 + COLUMN_DESCRIPTION + " TEXT, "
@@ -51,14 +56,46 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(createTasksTable);
     }
 
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
-        // Create tables again
+        // Usunięcie istniejącej tabeli
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        // Utworzenie nowej tabeli
         onCreate(db);
     }
+
+
+
+    void addTask(String title, String description, Category category) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        // Wstawienie wartości dla kolumny tytułu, opisu i kategorii
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_DESCRIPTION, description);
+        cv.put(COLUMN_CATEGORY, category.toString());
+
+        // Wstawienie aktualnej daty dla czasu utworzenia zadania
+        long currentTimeMillis = System.currentTimeMillis();
+        cv.put(COLUMN_CREATED_AT, currentTimeMillis);
+
+        // Ustawienie statusu na 0 (zadanie niezakończone)
+        cv.put(COLUMN_STATUS, 0);
+
+        cv.put(COLUMN_NOTIFICATION, 0);
+
+        // Wstawienie nowego zadania do bazy danych
+        long result = db.insert(TABLE_NAME, null, cv);
+        // Zamknięcie połączenia z bazą danych
+
+        if (result == -1) {
+            Toast.makeText(context, "Failed to add", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Added successfully!", Toast.LENGTH_SHORT).show();
+        }
+        db.close();
+    }
+
 }
 
 
